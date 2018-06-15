2decf6c14dfeb58bc6c009d10a7e86568017d61a
swapnachippagi <38342013+swapnachippagi@users.noreply.github.com>
2018-06-15 21:40:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,103 @@
-var sc2 = require('sc2-sdk');
+// var sc2 = require('sc2-sdk');
+// var api = sc2.Initialize({
+//   app: 'steemkar',
+//   callbackURL: 'https://swapnachippagi.github.io/index.html',
+//   scope: ['vote', 'comment']
+// });
+
+// var link = api.getLoginURL(state);
+// console.log(link)
+
+
 var api = sc2.Initialize({
-  app: 'steemkar',
+ app: 'steemkar',
   callbackURL: 'https://swapnachippagi.github.io/index.html',
   scope: ['vote', 'comment']
 });
 
-var link = api.getLoginURL(state);
-console.log(link)
+angular.module('app', [])
+  .config(['$locationProvider', function($locationProvider){
+    $locationProvider.html5Mode(true);
+  }])
+  .controller('Main', function($scope, $location, $http) {
+    $scope.loading = false;
+    $scope.parentAuthor = 'siol';
+    $scope.parentPermlink = '5vdmjq-test';
+    $scope.accessToken = $location.search().access_token;
+    $scope.expiresIn = $location.search().expires_in;
+    $scope.loginURL = api.getLoginURL();
+
+    if ($scope.accessToken) {
+      api.setAccessToken($scope.accessToken);
+      api.me(function (err, result) {
+        console.log('/me', err, result);
+        if (!err) {
+          $scope.user = result.account;
+          $scope.metadata = JSON.stringify(result.user_metadata, null, 2);
+          $scope.$apply();
+        }
+      });
+    }
+
+    $scope.isAuth = function() {
+      return !!$scope.user;
+    };
+
+    $scope.loadComments = function() {
+      steem.api.getContentReplies($scope.parentAuthor, $scope.parentPermlink, function(err, result) {
+        if (!err) {
+          $scope.comments = result.slice(-5);
+          $scope.$apply();
+        }
+      });
+    };
+
+    $scope.comment = function() {
+      $scope.loading = true;
+      var permlink = steem.formatter.commentPermlink($scope.parentAuthor, $scope.parentPermlink);
+      api.comment($scope.parentAuthor, $scope.parentPermlink, $scope.user.name, permlink, '', $scope.message, '', function(err, result) {
+        console.log(err, result);
+        $scope.message = '';
+        $scope.loading = false;
+        $scope.$apply();
+        $scope.loadComments();
+      });
+    };
+
+    $scope.vote = function(author, permlink, weight) {
+      api.vote($scope.user.name, author, permlink, weight, function (err, result) {
+        if (!err) {
+          alert('You successfully voted for @' + author + '/' + permlink);
+          console.log('You successfully voted for @' + author + '/' + permlink, err, result);
+          $scope.loadComments();
+        } else {
+          console.log(err);
+        }
+      });
+    };
+
+    $scope.updateUserMetadata = function(metadata) {
+      api.updateUserMetadata(metadata, function (err, result) {
+        if (!err) {
+          alert('You successfully updated user_metadata');
+          console.log('You successfully updated user_metadata', result);
+          if (!err) {
+            $scope.user = result.account;
+            $scope.metadata = JSON.stringify(result.user_metadata, null, 2);
+            $scope.$apply();
+          }
+        } else {
+          console.log(err);
+        }
+      });
+    };
+
+    $scope.logout = function() {
+      api.revokeToken(function (err, result) {
+        console.log('You successfully logged out', err, result);
+        delete $scope.user;
+        delete $scope.accessToken;
+        $scope.$apply();
+      });
+    };
+  });
