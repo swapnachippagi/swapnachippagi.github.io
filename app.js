20b49825a50d38e95b522263e6433e04750acba3
swapnachippagi <38342013+swapnachippagi@users.noreply.github.com>
2018-06-15 22:03:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,91 +15,96 @@ var api = sc2.Initialize({
   scope: ['vote', 'comment']
 });
 
-angular.module('app', [])
-  .config(['$locationProvider', function($locationProvider){
-    $locationProvider.html5Mode(true);
-  }])
-  .controller('Main', function($scope, $location, $http) {
-    $scope.loading = false;
-    $scope.parentAuthor = 'siol';
-    $scope.parentPermlink = '5vdmjq-test';
-    $scope.accessToken = $location.search().access_token;
- console.log($scope.accessToken);
-    $scope.expiresIn = $location.search().expires_in;
-    $scope.loginURL = api.getLoginURL();
+var link = api.getLoginURL(state);
+console.log(link)
 
-    if ($scope.accessToken) {
-      api.setAccessToken($scope.accessToken);
-      api.me(function (err, result) {
-        console.log('/me', err, result);
-       console.log("%%%%%%%%%%%%%%%%%%"+$scope.accessToken);
-        if (!err) {
-          $scope.user = result.account;
-          $scope.metadata = JSON.stringify(result.user_metadata, null, 2);
-          $scope.$apply();
-        }
-      });
-    }
 
-    $scope.isAuth = function() {
-      return !!$scope.user;
-    };
 
-    $scope.loadComments = function() {
-      steem.api.getContentReplies($scope.parentAuthor, $scope.parentPermlink, function(err, result) {
-        if (!err) {
-          $scope.comments = result.slice(-5);
-          $scope.$apply();
-        }
-      });
-    };
+// angular.module('app', [])
+//   .config(['$locationProvider', function($locationProvider){
+//     $locationProvider.html5Mode(true);
+//   }])
+//   .controller('Main', function($scope, $location, $http) {
+//     $scope.loading = false;
+//     $scope.parentAuthor = 'siol';
+//     $scope.parentPermlink = '5vdmjq-test';
+//     $scope.accessToken = $location.search().access_token;
+//  console.log($scope.accessToken);
+//     $scope.expiresIn = $location.search().expires_in;
+//     $scope.loginURL = api.getLoginURL();
 
-    $scope.comment = function() {
-      $scope.loading = true;
-      var permlink = steem.formatter.commentPermlink($scope.parentAuthor, $scope.parentPermlink);
-      api.comment($scope.parentAuthor, $scope.parentPermlink, $scope.user.name, permlink, '', $scope.message, '', function(err, result) {
-        console.log(err, result);
-        $scope.message = '';
-        $scope.loading = false;
-        $scope.$apply();
-        $scope.loadComments();
-      });
-    };
+//     if ($scope.accessToken) {
+//       api.setAccessToken($scope.accessToken);
+//       api.me(function (err, result) {
+//         console.log('/me', err, result);
+//        console.log("%%%%%%%%%%%%%%%%%%"+$scope.accessToken);
+//         if (!err) {
+//           $scope.user = result.account;
+//           $scope.metadata = JSON.stringify(result.user_metadata, null, 2);
+//           $scope.$apply();
+//         }
+//       });
+//     }
 
-    $scope.vote = function(author, permlink, weight) {
-      api.vote($scope.user.name, author, permlink, weight, function (err, result) {
-        if (!err) {
-          alert('You successfully voted for @' + author + '/' + permlink);
-          console.log('You successfully voted for @' + author + '/' + permlink, err, result);
-          $scope.loadComments();
-        } else {
-          console.log(err);
-        }
-      });
-    };
+//     $scope.isAuth = function() {
+//       return !!$scope.user;
+//     };
 
-    $scope.updateUserMetadata = function(metadata) {
-      api.updateUserMetadata(metadata, function (err, result) {
-        if (!err) {
-          alert('You successfully updated user_metadata');
-          console.log('You successfully updated user_metadata', result);
-          if (!err) {
-            $scope.user = result.account;
-            $scope.metadata = JSON.stringify(result.user_metadata, null, 2);
-            $scope.$apply();
-          }
-        } else {
-          console.log(err);
-        }
-      });
-    };
+//     $scope.loadComments = function() {
+//       steem.api.getContentReplies($scope.parentAuthor, $scope.parentPermlink, function(err, result) {
+//         if (!err) {
+//           $scope.comments = result.slice(-5);
+//           $scope.$apply();
+//         }
+//       });
+//     };
 
-    $scope.logout = function() {
-      api.revokeToken(function (err, result) {
-        console.log('You successfully logged out', err, result);
-        delete $scope.user;
-        delete $scope.accessToken;
-        $scope.$apply();
-      });
-    };
-  });
+//     $scope.comment = function() {
+//       $scope.loading = true;
+//       var permlink = steem.formatter.commentPermlink($scope.parentAuthor, $scope.parentPermlink);
+//       api.comment($scope.parentAuthor, $scope.parentPermlink, $scope.user.name, permlink, '', $scope.message, '', function(err, result) {
+//         console.log(err, result);
+//         $scope.message = '';
+//         $scope.loading = false;
+//         $scope.$apply();
+//         $scope.loadComments();
+//       });
+//     };
+
+//     $scope.vote = function(author, permlink, weight) {
+//       api.vote($scope.user.name, author, permlink, weight, function (err, result) {
+//         if (!err) {
+//           alert('You successfully voted for @' + author + '/' + permlink);
+//           console.log('You successfully voted for @' + author + '/' + permlink, err, result);
+//           $scope.loadComments();
+//         } else {
+//           console.log(err);
+//         }
+//       });
+//     };
+
+//     $scope.updateUserMetadata = function(metadata) {
+//       api.updateUserMetadata(metadata, function (err, result) {
+//         if (!err) {
+//           alert('You successfully updated user_metadata');
+//           console.log('You successfully updated user_metadata', result);
+//           if (!err) {
+//             $scope.user = result.account;
+//             $scope.metadata = JSON.stringify(result.user_metadata, null, 2);
+//             $scope.$apply();
+//           }
+//         } else {
+//           console.log(err);
+//         }
+//       });
+//     };
+
+//     $scope.logout = function() {
+//       api.revokeToken(function (err, result) {
+//         console.log('You successfully logged out', err, result);
+//         delete $scope.user;
+//         delete $scope.accessToken;
+//         $scope.$apply();
+//       });
+//     };
+//   });
